2d49ab796045f99e2e60a24a8a4295a3b245d2de
Steven Huggard <Sven@MBP15.hsd1.wa.comcast.net>
2016-09-26 15:13:08 -0700

    test commit
    Signed-off-by: Steven Huggard <Sven@MBP15.hsd1.wa.comcast.net>

--- apps/pokes/models.py
+++ apps/pokes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from ..users.models import User
 
 # Create your models here.
+class Poke_Manager(models.Manager):
+    def add_poke(self, pokee_id, poker_id):
+        pokee = User.objects.get(id=pokee_id)
+        poker = User.objects.get(id=poker_id)
+
+        poke = self.filter(pokee=pokee, poker=poker)
+
+        if poke:
+            poke[0].count += 1
+            poke[0].save()
+        else:
+            self.create(pokee=pokee, poker=poker, count=1)
+
+
+
+class Poke(models.Model):
+    poker = models.ForeignKey(User, related_name="poker")
+    pokee= models.ForeignKey(User, related_name="pokee")
+    count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    pokeMgr = Poke_Manager()
--- apps/pokes/views.py
+++ apps/pokes/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, redirect
 from ..users.models import User
+from .models import Poke
+from django.db.models import Sum
 
 # Create your views here.
 def index(request):
 	if "user_id" not in request.session:
 		return redirect("login_page")
 
+	print Poke.pokeMgr.all()
+
 	context = {
-		"curr_user": User.objects.get(id=request.session["user_id"])
+		"curr_user": User.objects.get(id=request.session["user_id"]),
+		"users": User.objects.exclude(id=request.session["user_id"]).annotate(total_count=Sum("pokee__count")),
+		# "users_poked_you": Poke.pokeMgr.filter(pokee=request.session["user_id"])
 	}
 
 	return render(request, "pokes/index.html", context)
+
+def create_poke(request, pokee_id):
+	poker_id = request.session["user_id"]
+	Poke.pokeMgr.add_poke(pokee_id, poker_id)
+	return redirect('index')
